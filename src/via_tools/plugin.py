a2748cdfa02c601fc0fc1f2c0aa45c00498cc6d9
Narottam Royal <narottamroyal@gmail.com>
2026-05-03 02:05:39 +1200

    Fix package path when running as script

--- src/via_tools/plugin.py
+++ src/via_tools/plugin.py
@@ -1,12 +1,22 @@
-from .gui import GUI
-from .engine import ViaTools
-
-
 def main() -> None:
+    # Delay import to ensure package is added to path if necessary
+    from .gui import GUI
+    from .engine import ViaTools
+
     engine = ViaTools()
     app = GUI(engine)
     app.run()
 
 
 if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    # Add package to path (necessary when running from KiCad)
+    file_path = Path(__file__).resolve().parent
+    __package__ = file_path.name
+    package_path = str(file_path.parent)
+    if package_path not in sys.path:
+        sys.path.insert(0, package_path)
+
     main()
